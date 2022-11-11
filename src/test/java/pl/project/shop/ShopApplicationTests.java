8a15d53f5cc0cacde4b94545f1bb7da3efc38ae7
nurys131 <nikt_nikowski@o2.pl>
2022-11-11 20:03:46 +0100

    initial commit

---
+++ src/test/java/pl/project/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.project.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
