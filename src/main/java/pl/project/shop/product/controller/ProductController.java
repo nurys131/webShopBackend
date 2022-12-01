fc7129f9f418113c2f05bd7fc4b732be90acf731
nurys131 <nikt_nikowski@o2.pl>
2022-12-01 13:38:43 +0100

    add paging of products

--- src/main/java/pl/project/shop/product/controller/ProductController.java
+++ src/main/java/pl/project/shop/product/controller/ProductController.java
@@ -1,12 +1,12 @@
 package pl.project.shop.product.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
 import pl.project.shop.product.model.Product;
 import pl.project.shop.service.ProductService;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -14,8 +14,9 @@ public class ProductController {
     private final ProductService productService;
 
     @GetMapping("/products")
-    public List<Product> getProducts() {
-        return productService.getProducts();
+    public Page<Product> getProducts(Pageable pageable) {
+        return productService.getProducts(pageable);
     }
+
 
 }
--- src/main/java/pl/project/shop/service/ProductService.java
+++ src/main/java/pl/project/shop/service/ProductService.java
@@ -1,18 +1,19 @@
 package pl.project.shop.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import pl.project.shop.product.model.Product;
 import pl.project.shop.repository.ProductRepository;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ProductService {
     private final ProductRepository productRepository;
-    public List<Product> getProducts() {
-        return productRepository.findAll();
+    public Page<Product> getProducts(Pageable pageable) {
+        return productRepository.findAll(pageable);
     }
 
 }
