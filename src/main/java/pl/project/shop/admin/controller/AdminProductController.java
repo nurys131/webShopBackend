26e4de5c1664540c8ff67319791b9a87a3d63a1f
nurys131 <nikt_nikowski@o2.pl>
2022-12-03 16:03:05 +0100

    add edit product

--- src/main/java/pl/project/shop/admin/controller/AdminProductController.java
+++ src/main/java/pl/project/shop/admin/controller/AdminProductController.java
@@ -3,19 +3,48 @@ package pl.project.shop.admin.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import pl.project.shop.admin.dto.AdminProductDto;
 import pl.project.shop.admin.model.AdminProduct;
 import pl.project.shop.admin.service.AdminProductService;
+
+import java.util.Locale;
 
 @RestController
 @RequiredArgsConstructor
 public class AdminProductController {
 
+    private static final Long EMPTY_ID = null;
     private final AdminProductService productService;
 
     @GetMapping("/admin/products")
-    public Page<AdminProduct> getProducts(Pageable pageable){
+    public Page<AdminProduct> getProducts(Pageable pageable) {
         return productService.getProducts(pageable);
+    }
+
+    @GetMapping("/admin/products/{id}")
+    public AdminProduct getProduct(@PathVariable Long id) {
+        return productService.getProduct(id);
+    }
+
+    @PostMapping("/admin/products")
+    public AdminProduct createProduct(@RequestBody AdminProductDto adminProductDto) {
+        return productService.createProduct(mapAdminProduct(adminProductDto, EMPTY_ID));
+    }
+
+    @PutMapping("/admin/products/{id}")
+    public AdminProduct updateProduct(@RequestBody AdminProductDto adminProductDto, @PathVariable Long id) {
+        return productService.updateProduct(mapAdminProduct(adminProductDto, id));
+    }
+
+    private AdminProduct mapAdminProduct(AdminProductDto adminProductDto, Long id) {
+        return AdminProduct.builder()
+                .id(id)
+                .name(adminProductDto.getName())
+                .category(adminProductDto.getCategory())
+                .description(adminProductDto.getDescription())
+                .price(adminProductDto.getPrice())
+                .currency(adminProductDto.getCurrency().toUpperCase(Locale.ROOT))
+                .build();
     }
 }
