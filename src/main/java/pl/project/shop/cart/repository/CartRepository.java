b11ef5292d8f9d957b71df9ac564ab42ce876d2b
nurys131 <nikt_nikowski@o2.pl>
2022-12-09 14:24:11 +0100

    add cart cleaning

--- src/main/java/pl/project/shop/cart/repository/CartRepository.java
+++ src/main/java/pl/project/shop/cart/repository/CartRepository.java
@@ -1,7 +1,18 @@
 package pl.project.shop.cart.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import pl.project.shop.cart.model.Cart;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface CartRepository extends JpaRepository<Cart, Long> {
+
+    List<Cart> findByCreatedLessThan(LocalDateTime minusDays);
+
+    @Query("delete from Cart c where c.id in (:ids)")
+    @Modifying
+    void deleteAllByIdIn(List<Long> ids);
 }
---
+++ src/main/java/pl/project/shop/cart/service/CartCleanupService.java
@@ -0,0 +1,36 @@
+package pl.project.shop.cart.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import pl.project.shop.cart.model.Cart;
+import pl.project.shop.cart.repository.CartItemRepository;
+import pl.project.shop.cart.repository.CartRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class CartCleanupService {
+
+    private final CartRepository cartRepository;
+    private final CartItemRepository cartItemRepository;
+
+    @Transactional
+    @Scheduled(cron = "${app.cart.cleanup.expression}")
+    public void cleanupOldCarts() {
+        List<Cart> carts = cartRepository.findByCreatedLessThan(LocalDateTime.now().minusDays(3));
+        List<Long> ids = carts.stream()
+                .map(Cart::getId)
+                .toList();
+        log.info("Stare koszyki " + carts.size());
+        if(!ids.isEmpty()) {
+            cartItemRepository.deleteAllByCartIdIn(ids);
+            cartRepository.deleteAllByIdIn(ids);
+        }
+    }
+}
