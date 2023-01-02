958d8a0e78396f949ed5c49cc237505accef0659
Hubert Furtak <HubertFurtak99@gmail.com>
2023-01-02 14:00:40 +0100

    Add change order statuses for admin

--- src/main/java/pl/project/shop/admin/order/controller/AdminOrderController.java
+++ src/main/java/pl/project/shop/admin/order/controller/AdminOrderController.java
@@ -3,14 +3,16 @@ package pl.project.shop.admin.order.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import pl.project.shop.admin.order.controller.dto.AdminInitDataDto;
 import pl.project.shop.admin.order.controller.dto.AdminOrderDto;
 import pl.project.shop.admin.order.controller.mapper.AdminOrderMapper;
 import pl.project.shop.admin.order.model.AdminOrder;
+import pl.project.shop.admin.order.model.AdminOrderStatus;
 import pl.project.shop.admin.order.service.AdminOrderService;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/admin/orders")
@@ -27,5 +29,23 @@ public class AdminOrderController {
     @GetMapping("/{id}")
     public AdminOrder getOrders(@PathVariable Long id) {
         return adminOrderService.getOrder(id);
+    }
+
+    @PatchMapping("/{id}")
+    public void patchOrder(@PathVariable Long id, @RequestBody Map<String, String> values) {
+        adminOrderService.patchOrder(id, values);
+    }
+
+    @GetMapping("/initData")
+    public AdminInitDataDto getInitData() {
+        return new AdminInitDataDto(createOrderStatusesMap());
+    }
+
+    private Map<String, String> createOrderStatusesMap() {
+        HashMap<String, String> statuses = new HashMap<>();
+        for (AdminOrderStatus value : AdminOrderStatus.values()) {
+            statuses.put(value.name(), value.getValue());
+        }
+        return statuses;
     }
 }
--- src/main/java/pl/project/shop/admin/order/model/AdminOrderStatus.java
+++ src/main/java/pl/project/shop/admin/order/model/AdminOrderStatus.java
@@ -1,5 +1,21 @@
 package pl.project.shop.admin.order.model;
 
 public enum AdminOrderStatus {
-    NEW
+    NEW("Nowe"),
+    PAID("Opłacone"),
+    PROCESSING("Przetwarzane"),
+    WAITING_FOR_DELIVERY("Oczekuje na dostawę"),
+    COMPLETED("Zrealizowane"),
+    CANCELED("Anulowane"),
+    REFUND("Zwrócone");
+
+    private String value;
+
+    AdminOrderStatus(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
 }
